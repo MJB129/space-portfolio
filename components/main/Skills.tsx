0d129d76eadc461c925dbf6e25243cc817fafa51
MJB129 <romedigital2.0@gmail.com>
2024-02-05 13:12:08 -0500

    added three.js and started working on adding 3D webcards that turn over to reveal a description of the projects completed

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -8,7 +8,7 @@ const Skills = () => {
     <section 
       id="skills"
       className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
-      style={{ transform: "scale(0.9" }}
+      style={{ transform: "scale(0.9)" }}
     >
       <SkillText /><div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
         {Skill_data.map((image, index) => (
